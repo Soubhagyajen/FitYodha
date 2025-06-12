4fd2f83d4f7f31d5c5640a76d67bcb25e3af274f
Soubhagyajen <soubhagyajena834@gmail.com>
2025-06-12 13:23:11 +0530

    Connected to Render PostgreSQL

--- fityodha/settings.py
+++ fityodha/settings.py
@@ -110,7 +110,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # ✅ fallback for local dev
+        conn_max_age=600
+    )
 }
 
 
